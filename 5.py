38d40c4bbd3f96a9a7504f8864e6fbe6f4b521bf
1553238-max <ilyazhikharev1553238@gmail.com>
2025-11-11 21:39:46 +0300

    Add files via upload

---
+++ 5.py
@@ -0,0 +1,15 @@
+num=42
+name='Bob'
+if num>30:
+    if num<50:
+        print('случай 1')
+    elif name >'Markus':
+        print('случай 2')
+    else:
+        print('случай 3')
+elif name <'Markus':
+    print('случай 4')
+elif num !=42:
+    print('случай 5')
+else:
+    print('случай 6')
